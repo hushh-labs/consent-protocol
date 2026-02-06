44dd7797e283ebafef0ae2ee7f0454f5d11421a3
Kushal Trivedi <kushaltrivedi54@gmail.com>
2026-02-06 06:54:22 -0800

    fix(ui): native layout polish, chart colors, animations

--- hushh-webapp/app/template.tsx
+++ hushh-webapp/app/template.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 export default function Template({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+
   return (
-    <div className="flex-1 flex flex-col h-full animate-in fade-in slide-in-from-bottom-4 duration-700">
+    <div 
+      key={pathname}
+      className="flex-1 flex flex-col animate-in fade-in slide-in-from-bottom-4 duration-700"
+    >
       {children}
     </div>
   );
